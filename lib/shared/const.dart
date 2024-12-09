a7ee1ac472e9ec34a56a0cbf1007ecfbbb6a14d5
febeFlo <122088260+febeFlo@users.noreply.github.com>
2024-12-09 17:18:14 +0700

    Delete API KEY

--- lib/shared/const.dart
+++ lib/shared/const.dart
@@ -2,5 +2,5 @@ part of 'shared.dart';
 
 class Const {
   static String baseUrl = 'api.rajaongkir.com';
-  static String apiKey = '855813b222b10d90e5d3ec901f0d8220';
+  static String apiKey = 'API_KEY';
 }
